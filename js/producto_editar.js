f040c00eedb07e48d1e5e0a00fea78d66838a209
GalvanSergio23026 <129911483+GalvanSergio23026@users.noreply.github.com>
2023-07-16 17:15:43 -0300

    Update producto_editar.js

--- js/producto_editar.js
+++ js/producto_editar.js
@@ -47,7 +47,7 @@ redirect: 'follow'
 fetch(this.url, options)
 .then(function () {
 alert("Registro modificado")
-window.location.href = "../templates/index.html";
+window.location.href = "../templates/Producto.html";
 })
 .catch(err => {
 console.error(err);
